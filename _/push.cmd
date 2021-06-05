f75029df3b7c8f5a61cc19ed8dc71e94375aae07
T. Korn <email@tobiaskorn.de>
2021-06-05 19:57:10 +0200

    added separate libs containing models (for .NET)

--- _/push.cmd
+++ _/push.cmd
@@ -1,13 +1,13 @@
 
-xcopy ".\BillingData\ORSCF-BillingData.Schema.json" "..\BillingData\" /d /r /y /s
-copy ".\BillingData\ORSCF-BillingData.md" "..\BillingData\readme.md" /Y
+xcopy "EDMX\BillingData\ORSCF-BillingData.Schema.json" "..\BillingData\" /d /r /y /s
+copy "EDMX\BillingData\ORSCF-BillingData.md" "..\BillingData\readme.md" /Y
 
-xcopy ".\IdentityManagement\ORSCF-IdentityManagement.Schema.json" "..\IdentityManagement\" /d /r /y /s
-copy ".\IdentityManagement\ORSCF-IdentityManagement.md" "..\IdentityManagement\readme.md" /Y
+xcopy "EDMX\IdentityManagement\ORSCF-IdentityManagement.Schema.json" "..\IdentityManagement\" /d /r /y /s
+copy "EDMX\IdentityManagement\ORSCF-IdentityManagement.md" "..\IdentityManagement\readme.md" /Y
 
-xcopy ".\StudyWorkflowDefinition\ORSCF-StudyWorkflowDefinition.Schema.json" "..\StudyWorkflowDefinition\" /d /r /y /s
-copy ".\StudyWorkflowDefinition\ORSCF-StudyWorkflowDefinition.md" "..\StudyWorkflowDefinition\readme.md" /Y
+xcopy "EDMX\StudyWorkflowDefinition\ORSCF-StudyWorkflowDefinition.Schema.json" "..\StudyWorkflowDefinition\" /d /r /y /s
+copy "EDMX\StudyWorkflowDefinition\ORSCF-StudyWorkflowDefinition.md" "..\StudyWorkflowDefinition\readme.md" /Y
 
-xcopy ".\VisitData\ORSCF-VisitData.Schema.json" "..\VisitData\" /d /r /y /s
-copy ".\VisitData\ORSCF-VisitData.md" "..\VisitData\readme.md" /Y
+xcopy "EDMX\VisitData\ORSCF-VisitData.Schema.json" "..\VisitData\" /d /r /y /s
+copy "EDMX\VisitData\ORSCF-VisitData.md" "..\VisitData\readme.md" /Y
 
